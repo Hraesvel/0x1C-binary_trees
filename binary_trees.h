5a7f42c4221c6f5e561ae1c09b95bfdc3d287921
Martin Smith <msmith3dart@icloud.com>
2019-03-12 17:50:39 +0000

    add proto depth and style fix

--- binary_trees.h
+++ binary_trees.h
@@ -12,10 +12,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 /* STRUCTS */
@@ -35,4 +35,5 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 #endif
