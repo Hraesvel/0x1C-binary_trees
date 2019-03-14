4e7770f87a7bbcf2a8cd7ff9964bcb3040d67809
Martin Smith <msmith3dart@icloud.com>
2019-03-14 23:02:51 +0000

    betty fixes

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,11 +1,24 @@
 #include "binary_trees.h"
 #include <stdio.h>
 
+/**
+ * min - find the smallest of two values
+ * @a: left side
+ * @b: right side
+ *
+ * Return: smallest value (INT)
+ */
 int min(int a, int b)
 {
 	return ((a <= b) ? a : b);
 }
 
+/**
+ * is_full - Logic to check if tree is full
+ * @tree: pointer to a binary tree
+ *
+ * Return: 1 if full else 0
+ */
 int is_full(const binary_tree_t *tree)
 {
 
@@ -17,12 +30,12 @@ int is_full(const binary_tree_t *tree)
 		is_full(tree->left);
 	if (tree->right)
 		is_full(tree->right);
-	
+
 	return (1);
-		
+
 }
 /**
- * binary_tree_is_full - check if tree is full
+ * binary_tree_is_full - Helper function to check if tree is full
  * @tree: pointer to a binary tree
  *
  * Return: 1 if full else 0
