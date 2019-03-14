6bfe5a541085b91e88dde95f084fd450f3fc6003
Martin Smith <msmith3dart@icloud.com>
2019-03-14 22:25:13 +0000

    return to work
    had modify to test Holberton checker

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = NULL;
-	if(!parent)
+	if (!parent)
 		return (NULL);
 	node = binary_tree_node(parent, value);
 	if (parent->left)
